fbb5d07be240d3afc7d39c8e1d7b4834cce39bcc
kimas8833 <38730129+kimas8833@users.noreply.github.com>
2024-08-13 22:13:15 +0900

    [Silver IV] Title: 큐 2, Time: 368 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    unsigned int a;
+    queue<int> q;
+    string st;
+    while(N--)
+    {
+        cin >> st;
+        if(st == "push")
+        {
+            cin >> a;
+            q.push(a);
+        }
+        else if(st == "pop")
+        {
+            if(q.empty())
+                cout << "-1\n";
+            else
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+                
+        }
+        else if(st == "size")
+        {
+            cout << q.size() << '\n';
+        }
+        else if(st == "empty")
+        {
+            cout << q.empty() << '\n';
+        }
+        else if(st == "front")
+        {
+            if(q.empty())
+                cout << "-1\n";
+            else
+                cout << q.front() << '\n';
+        }
+        else if(st == "back")
+        {
+            if(q.empty())
+                cout << "-1\n";
+            else
+                cout << q.back() << '\n';
+        }
+        
+
+
+    }
+
+}
