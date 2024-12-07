69e71ffa916584a3a7ad3fe3d9ca6c3b1e9abeed
kimas8833 <38730129+kimas8833@users.noreply.github.com>
2024-12-07 11:17:44 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    priority_queue<int> maxHeap;
+    
+    for(int i=0;i<N; ++i)
+    {
+        int x;
+        cin >> x;
+        if( x == 0)
+        {
+            if(maxHeap.empty())
+            {
+                cout << 0 << '\n';
+            }
+            else
+            {
+                cout << maxHeap.top() << '\n';
+                maxHeap.pop();
+            }
+        }
+        else
+        {
+            maxHeap.push(x);
+        }
+    }
+    return 0;
+}
