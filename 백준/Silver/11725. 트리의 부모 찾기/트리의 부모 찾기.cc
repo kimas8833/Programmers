77935de9d0ac4d80283e2271a45a043a078face1
kimas8833 <38730129+kimas8833@users.noreply.github.com>
2024-12-07 23:10:40 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
     int N;
     cin >> N;
     vector<vector<int>> graph(N+1);
-    vector<int> parent(N+1,0);
+    vector<int> parent(N+1, 0);
+    
     for(int i=0;i<N-1;i++)
     {
         int x,y;
         cin >> x >> y;
         graph[x].push_back(y);
-        graph[y].push_back(x);
-        
+        graph[y].push_back(x);      
     }
     
     queue<int> q;
@@ -38,12 +40,9 @@ int main()
             }
         }
     }
-    
-    for(int i=2;i<=N;++i)
+    for(int i=2;i<=N;i++)
     {
         cout << parent[i] << '\n';
     }
     return 0;
-    
-    
 }
