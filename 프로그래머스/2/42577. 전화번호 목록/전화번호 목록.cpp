0ed46b16ec612084f160c8e4fd7a846f65a45b1e
kimas8833 <38730129+kimas8833@users.noreply.github.com>
2025-04-24 21:27:09 +0900

    [level 2] Title: 전화번호 목록, Time: 71.00 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -7,8 +7,9 @@ bool solution(vector<string> phone_book) {
     sort(phone_book.begin(), phone_book.end());
     for(int i=0;i<phone_book.size()-1;i++)
     {
-        if(phone_book[i+1].find(phone_book[i])==0)
+        if(phone_book[i+1].find(phone_book[i])== 0)
             return false;
     }
+
     return true;
 }
