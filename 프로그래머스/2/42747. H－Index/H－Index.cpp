6f3bf12bbeb64b1bceb083cb16208b4cd440741c
kimas8833 <38730129+kimas8833@users.noreply.github.com>
2025-04-24 22:41:08 +0900

    [level 2] Title: H-Index, Time: 0.08 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -3,11 +3,15 @@
 #include <algorithm>
 using namespace std;
 
+bool compare(const int&a, const int&b)
+{
+    return a > b;
+}
+
+
 int solution(vector<int> citations) {
     int answer = 0;
-    sort(citations.begin(), citations.end(), [](const int& a, const int& b){
-        return a>b;
-    });
+    sort(citations.begin(), citations.end(), compare);
     for(int i=0;i<citations.size();i++)
     {
         if(citations[i] <= i)
