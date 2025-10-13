a6270d458ca6a29e204e6c9a7e6adcbf35b498e3
kimas8833 <38730129+kimas8833@users.noreply.github.com>
2025-10-13 22:26:35 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "";
+    int cards1_idx =0;
+    int cards2_idx =0;
+    vector<string> ans;
+    for(int i=0;i<goal.size();i++)
+    {
+        if(goal[i] == cards1[cards1_idx] && cards1_idx <cards1.size())
+        {
+            cards1_idx++;
+        }
+        else if(goal[i] == cards2[cards2_idx] && cards2_idx <cards2.size())
+        {
+            cards2_idx++;
+        }
+        else 
+            return "No";
+        
+    }
+    return "Yes";
+
+}
